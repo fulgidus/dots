021469c07facb780369e3e2288178f5362b279ca
Alessio Corsi <alessio.corsi@gmail.com>
2026-04-25 01:27:22 +0200

    feat: migrate dotfiles from stow to chezmoi
    - zsh: .zshrc, .zshenv, .p10k.zsh
    - terminal: .wezterm.lua
    - git: .gitconfig
    - vscode: settings.json
    - fix: hardcoded brew paths → runtime detection (Linux/macOS portable)
    - fix: hardcoded /home/fulgidus paths → $HOME

---
+++ dot_wezterm.lua
@@ -0,0 +1,69 @@
+local wez = require("wezterm")
+local conf = wez.config_builder()
+
+-- General
+conf.line_height = 1.0
+conf.font = wez.font("FiraCode Nerd Font Mono")
+conf.window_background_opacity = 0.8
+conf.color_scheme = "Tokyo Night"
+conf.default_cursor_style = "SteadyUnderline"
+conf.scrollback_lines = 10000
+
+
+-- Copy to clipboard on mouse select
+conf.mouse_bindings = {
+    {
+        event = { Up = { streak = 1, button = "Left" } },
+        mods = "NONE",
+        action = wez.action.CompleteSelectionOrOpenLinkAtMouseCursor "ClipboardAndPrimarySelection",
+    },
+}
+
+conf.window_decorations = "TITLE | RESIZE"
+conf.enable_tab_bar = false
+conf.window_close_confirmation = "NeverPrompt"
+
+-- Key bindings
+conf.keys = {
+    -- Chiudi pane
+    {
+        key = "x",
+        mods = "SUPER|SHIFT",
+        action = wez.action.CloseCurrentPane { confirm = false },
+    },
+    -- Split verticale (divide a destra)
+    {
+        key = "RightArrow",
+        mods = "CTRL|ALT",
+        action = wez.action.SplitHorizontal { domain = "CurrentPaneDomain" },
+    },
+    -- Split orizzontale (divide in basso)
+    {
+        key = "DownArrow",
+        mods = "CTRL|ALT",
+        action = wez.action.SplitVertical { domain = "CurrentPaneDomain" },
+    },
+    -- Navigazione tra pane
+    {
+        key = "LeftArrow",
+        mods = "ALT",
+        action = wez.action.ActivatePaneDirection "Left",
+    },
+    {
+        key = "RightArrow",
+        mods = "ALT",
+        action = wez.action.ActivatePaneDirection "Right",
+    },
+    {
+        key = "UpArrow",
+        mods = "ALT",
+        action = wez.action.ActivatePaneDirection "Up",
+    },
+    {
+        key = "DownArrow",
+        mods = "ALT",
+        action = wez.action.ActivatePaneDirection "Down",
+    },
+}
+
+return conf
